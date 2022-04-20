5fe5cc90874d2862f495537e67c8b8dc2f483ee0
Brian Lee <brianlee1597@gmail.com>
2022-04-20 21:43:59 +0900

    error unit tests

--- contracts/redirection/src/tests/tests.rs
+++ contracts/redirection/src/tests/tests.rs
@@ -1,12 +1,14 @@
 use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info, MOCK_CONTRACT_ADDR};
 use cosmwasm_std::{attr, coins, CosmosMsg, Addr, Api};
+use crate::ContractError;
+use crate::contract::execute;
+use crate::{msg::InstantiateMsg, contract::instantiate};
 
-use crate::msg::QueryMsg;
+use crate::msg::{QueryMsg, ExecuteMsg};
 use crate::query::query;
 
 #[test]
 fn proper_initialization() {
-    use crate::{msg::InstantiateMsg, contract::instantiate};
     let mut deps = mock_dependencies(&[]);
     let sample_addr = Addr::unchecked("terra1757tkx08n0cqrw7p86ny9lnxsqeth0wgp0em95");
     let instantiate_msg = InstantiateMsg {
@@ -20,4 +22,24 @@ fn proper_initialization() {
     let result = 
     instantiate(deps.as_mut(), mock_env(), info, instantiate_msg.clone()).unwrap();
     assert_eq!(0, result.messages.len());
+}
+
+#[test]
+fn deposit_new_pool() {
+    let mut deps = mock_dependencies(&[]);
+    let sample_addr = Addr::unchecked("terra1757tkx08n0cqrw7p86ny9lnxsqeth0wgp0em95");
+    let info = mock_info(sample_addr.as_ref(), &coins(10000000, "uusd"));
+
+    let msg = ExecuteMsg::DepositPool { percentage: 2 }; //percentage should be between 5 and 100;
+    let res = execute(deps.as_mut(), mock_env(), info, msg);
+
+    //should throw percentage error;
+    assert_eq!(ContractError::WrongPercentageInput {}, res.unwrap_err());
+
+    let info = mock_info(sample_addr.as_ref(), &coins(100, "uusd"));
+    let msg = ExecuteMsg::DepositPool { percentage: 50 };
+    let res = execute(deps.as_mut(), mock_env(), info, msg);  
+    
+    //should throw low coin error;
+    assert_eq!(ContractError::MakeNewPoolError {}, res.unwrap_err());
 }
